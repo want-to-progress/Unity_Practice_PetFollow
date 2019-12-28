6261379893da6170bcc53d36e4a597eb665389fb
YUN <32327398+want-to-progress@users.noreply.github.com>
2019-12-28 14:16:45 +0800

    add dragon following

---
+++ UnityPetFollow/Assets/Scripts/dragon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragon : MonoBehaviour
+{
+    private GameObject dragonPos;
+    private GameObject human;
+    
+
+    void Start()
+    {
+        dragonPos = GameObject.Find("飛龍");
+        human = GameObject.Find("野蠻人");
+    }
+
+    
+    void Update()
+    {
+        if (Vector3.Distance(dragonPos.transform.position,
+                             human.transform.position) > 2)
+        {
+            Vector3 newDragonPos = Vector3.Lerp(dragonPos.transform.position,
+                                                human.transform.position,
+                                                0.5f * Time.deltaTime);
+            //newDragonPos.z = dragonPos.transform.position.z;
+
+            dragonPos.transform.position = newDragonPos;
+        }
+        dragonPos.transform.LookAt(human.transform);
+    }
+}
